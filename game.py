7fa92fe8caac99a32739d906da7fa08a31a78301
Eileen Hengel <eileenhengel@10-16-188-178.dynapool.wireless.nyu.edu>
2019-06-05 18:57:21 -0400

    Update game.py
    Updated game py

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+# CAPTURE INPUTS
+
+user_choice = input("Please select one of the following options: 'rock' , 'paper', or 'scissors'")
+
+print("-------------------")
+
+# VALIDATE INPUTS
+
+# GENERATE COMPUTER SELECTION
+
+# DETERMINE THE WINNER
+
+# DISPLAY FINAL OUTPUTS / OUTCMOES
+
