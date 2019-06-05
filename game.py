12ce97f5d29c86ad7c1cd37be207e4533b7546dd
Eileen Hengel <eileenhengel@10-16-188-178.dynapool.wireless.nyu.edu>
2019-06-05 19:26:30 -0400

    generate comp choice

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 # game.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # CAPTURE INPUTS
@@ -12,15 +14,17 @@ print("USER CHOICE", user_choice)
 
 # VALIDATE INPUTS
 
-if user_choice in ["rock", "paper", "scissors"]:
-    print ("VALID")
-else:
+if user_choice not in ["rock", "paper", "scissors"]:
     print ("INVALID SELECTION. PLEASE TRY AGAIN...")
     exit()
 
 # GENERATE COMPUTER SELECTION
 
+computer_choice = random.choice(["rock", "paper", "scissors"])
+
+print("-------------------")
 print("GENERATING...")
+print("COMPUTER CHOICE", computer_choice)
 
 # DETERMINE THE WINNER
 
