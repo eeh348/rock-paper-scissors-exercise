4b035b36de3fb23e2a81f37cc52493ae03474207
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-09 08:46:04 -0400

    Update testing

--- game.py
+++ game.py
@@ -2,6 +2,67 @@
 
 import random
 
+def my_message():
+    return "HELLO"
+
+def determine_winner(user_choice, computer_choice):
+    return "PASS"
+
+
+#if this script is executed from the cmd line
+if __name__ == "__main__":
+
+    print("Rock, Paper, Scissors, Shoot!")
+
+    # CAPTURE INPUTS
+
+    user_choice = input("Please select one of the following options: 'rock' , 'paper', or 'scissors'")
+
+    print("-------------------")
+
+    print("USER CHOICE", user_choice)
+
+    # VALIDATE INPUTS
+
+    options = ["rock", "paper", "scissors"]
+
+    if user_choice not in options:
+        print ("INVALID SELECTION. PLEASE TRY AGAIN...")
+        exit()
+
+    # GENERATE COMPUTER SELECTION
+
+    computer_choice = random.choice(options)
+
+    print("-------------------")
+    print("GENERATING...")
+    print("COMPUTER CHOICE", computer_choice)
+
+    # DETERMINE THE WINNER
+
+    #user_choice = "rock"
+    #computer_choice = "rock"
+
+    if user_choice == computer_choice:
+        print ("Tie!", user_choice, "equals", computer_choice)
+    elif user_choice == "rock":
+        if computer_choice == "paper":
+            print ("You lose!", computer_choice, "covers", user_choice)
+        else:
+            print ("You win!", user_choice, "smashes", computer_choice)
+    elif user_choice == "paper":
+        if computer_choice == "scissors":
+            print ("You lose!", computer_choice, "cuts",  user_choice)
+        else:
+            print ("You win!", user_choice, "covers", computer_choice)
+    elif user_choice == "scissors":
+        if computer_choice == "rock":
+            print ("You lose!", computer_choice, "smashes", user_choice)
+        else: 
+            print ("You win!", user_choice, "cuts",computer_choice)
+        pass
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # CAPTURE INPUTS
